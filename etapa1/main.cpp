71f18811583f4c143da6cbf82e5fc1bd12f548fe
Bianca Pelegrini <bmpelegrini@gmail.com>
2024-10-13 01:14:46 -0300

    some cleanups

--- etapa1/main.cpp
+++ etapa1/main.cpp
@@ -10,6 +10,12 @@ int yylex();
 
 int main(int argc, char **argv)
 {
+    if (argc < 2)
+    {
+        printf("\033[31mError: no file provided for analysis.\033[0m\nExample usage: \033[34m$ ./etapa1 the_source_code.txt\033[0m\n");
+        return 1;
+    }
+
     initMe();
     extern FILE *yyin;
     yyin = fopen(argv[1], "r");
@@ -18,8 +24,9 @@ int main(int argc, char **argv)
     while (isRunning())
     {
         token = yylex();
+        printf(" TOKEN %d\n", token);
     }
 
-    printf("Total lines: %d\n", getLineNumber());
+    printf("Total lines analyzed: %d\n", getLineNumber());
     return 0;
 }
